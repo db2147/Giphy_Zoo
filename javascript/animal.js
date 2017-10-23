09df8b01e1522c50827c827a3a0763338cd9fd7f
David Bierma <31113045+db2147@users.noreply.github.com>
2017-10-23 11:23:37 -0500

    <fixjquerylink>

--- javascript/animal.js
+++ javascript/animal.js
@@ -1,9 +1,9 @@
-var animalButtons
+var animalButtons;
 			//web URL with api key
 var queryURL = "https://giphy.com/search/animals&apikey=tFriDfADykrYHsiVZS1J7YhZimeGAIBQ";
 
-var animal input
-var add animal
+var animalInput = "";
+var addAnimal = "";
 var topics = ["birds", "monkeys", "bears", "pigs", "dog", "cat", "rabbit", "turtle", "gophur"];
 
 // Function for displaying movie data
@@ -14,9 +14,10 @@ $("#animalButtons").empty();
 
 // Looping through the array of movies
 for (var i = 0; i < topics.length; i++) {
-
-//this code is all jquery needs to create the start and end tag (<button></button)
-var a = $("<button>");
+	// create buttons
+	var a = $("<button>"); //created the button var in jquery
+	// append topics[i] to button data-attribute
+	// show button topics[i] text value 
 //adding a class
 a.addClass("animal")
 // Adding a data-attribute with a value of the movie at index i
@@ -28,12 +29,18 @@ $("#animalButtons").append(a);
 	}
 }
 // This function handles events where one button is clicked
-$("#add-animal").on("click"), function(event) {
+$("#addanimal").on("click"), function(event) {
 //event.preventDefault() prevents the form from trying to submit itself.
 //We're using a form so that the user can hit enter instead of clicking the button if they want
 event.preventDefault();
 
+// This line will grab the text from the input box
+var animal = $("#animalinput").val().trim();
+// The movie from the textbox is then added to our array
+topics.push(animas);
 
+//calling renderButtons which handle the processing of our movie array
+renderButtons();
+};
 
-
-}
+ 
