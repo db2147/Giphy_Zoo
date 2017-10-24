bdefb8d34830aa0bb66de6edc8a58f002f4fef3c
David Bierma <31113045+db2147@users.noreply.github.com>
2017-10-24 14:56:03 -0500

    <trial-and-error>

--- javascript/animal.js
+++ javascript/animal.js
@@ -37,11 +37,19 @@ function renderButtons() {
 }
 
 // This function handles events where one button is clicked
-$("#addAnimal").on("click"),
+$("#addAnimal").on("click"), 
     function(event) {
         //event.preventDefault() prevents the form from trying to submit itself.
         //We're using a form so that the user can hit enter instead of clicking the button if they want
         event.preventDefault();
+        // This line grabs the input from the textbox
+        var animal = $("#animal-input").val().trim();
+        //Adding the animal from the textbox to our array
+        topics.push(animal);
+        // Calling renderButtons which handles the processing of our movie array
+        renderButtons();
+        console.log();
+
 
     }
 
